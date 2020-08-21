ed1fe48926feac5e8892cc0b17da79535798f0f3
AidanMcB <aidankmcbride@gmail.com>
2020-08-20 22:15:30 -0400

    uploading to git

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [color, setColor] = useState('')
+
+  const changeColor = () => {
+    const red = Math.floor(Math.random() * 256)
+    const green = Math.floor(Math.random() * 256)
+    const blue = Math.floor(Math.random() * 256)
+
+    const newColor = `rgb(${red},${green},${blue})`
+    setColor(newColor)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{
+      backgroundColor: color,
+      height: "100vh",
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+    }}>
+      <button style={{
+        borderRadius:"25px",
+        fontSize: "18px",
+      outline:"none"
+      }}
+      onClick={() => changeColor()} >Click Me!</button>
     </div>
   );
 }
